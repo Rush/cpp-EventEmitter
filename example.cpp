2bf31cf85b838dcd0b5c8c1095d1ab1b97c53cc8
Damian Kaczmarek <rush@rushbase.net>
2013-09-23 03:22:39 +0200

    Add small example.

---
+++ example.cpp
@@ -0,0 +1,31 @@
+#include "EventEmitter.hpp"
+
+// Suggested non-macro usage
+class Widget {
+public:
+	EventEmitter<int, int> doubleIntEvent;
+};
+
+
+// Suggested macro usage
+DefineEventEmitter(DoubleInt, int, int);
+class Widget2 : public DoubleIntEventEmitter {
+	
+};
+
+
+int main()
+{
+	Widget w;	
+	w.doubleIntEvent.on([=](int a, int b) { // slot
+		
+	});
+	w.doubleIntEvent.trigger(123, 213); // signal
+	
+	Widget2 w2;
+	
+	w2.onDoubleInt([=](int a, int b) {
+		
+	});
+	w2.triggerDoubleInt(213, 233);
+}
